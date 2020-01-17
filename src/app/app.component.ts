9f353b6c4c13436a4e740a30f877ecee21f6f5f9
Alejandro <alejandro1996rd@gmail.com>
2020-01-17 09:43:20 +0100

    app angular inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+/**componente principal que se encarga de arrancar la app
+ * 
+ * Selector: nombre de la etiqueta para inyectar el componente en html.
+ * templateUrl: es la vista donde se ejecuta el codigo html.
+ * styleUrls: es la vista que se usa para dar formato a nuestra pagina html.
+ * 
+*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
